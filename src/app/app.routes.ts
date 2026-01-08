f44ce29a1e5d1e962655c070730ebbf1164ddf8e
Roman Fiodorov <roman.f@kyla.com>
2026-01-08 23:00:26 +0200

    Add About page route, footer, and styling to app layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game/ui/game.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'game', pathMatch: 'full' },
-  { path: 'game', component: GameComponent }
+  { path: 'game', component: GameComponent },
+  { path: 'about', component: AboutComponent }
 ];
